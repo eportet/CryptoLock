d084b88d1cc5635b323d2f715d951bd4ac195f41
Eduardo Portet <eportet@dhcp-wifi-8021x-155-41-23-103.bu.edu>
2018-04-25 15:45:46 -0400

    Add RFID and Motor functionality

--- node/gatekeep.js
+++ node/gatekeep.js
@@ -1,13 +1,15 @@
 var Gpio = require('onoff').Gpio;
 var greenled = new Gpio(23, 'out');
 var redled = new Gpio(24, 'out');
-var button = new Gpio(18, 'in', 'falling', {debounceTimeout: 20});
-
-var CARDSCAN = "0xa1b29Ae29C6A877383Ad50F5a66EE2Ef0a75e84E";
+var pn532 = require('pn532');
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyS0', { baudRate: 115200 });
+var rfid = new pn532.PN532(port);
+var ndef = require('ndef');
+var spawn = require("child_process").spawn;
 
 redled.writeSync(1);
 greenled.writeSync(0);
-console.log('**** GATE CLOSED ****');
 
 var Web3 = require('web3');
 var web3 = new Web3();
@@ -29,30 +31,49 @@ var Gatekeeper = web3.eth.contract(GatekeeperABI).at(GatekeeperContractAddress);
 
 console.log('\n********* READY *********');
 
-button.watch(function (err, value) {
-	if (err) {
-		console.error(err);
-		return;
-	}
+function redLight(i) {
+	setTimeout(function() { redled.writeSync((i+1)%2); }, 250*i)
+}
 
-	var isValid = Gatekeeper.authorizeAddress(CARDSCAN);
-	if (isValid) {
-		console.log("**** OPEN GATE ****");
-		redled.writeSync(0);
-		greenled.writeSync(1);
-		setTimeout(function() {
-			console.log("**** CLOSE GATE ****");
-			redled.writeSync(1);
-			greenled.writeSync(0);
-		}, 5000)
-	} else {
-		console.log("***** INVALID *****");
-	}
+rfid.on('ready', function() {
+    console.log('Listening for a tag scan...');
+    rfid.on('tag', function(tag) {
+        console.log('Reading tag data...');
+        rfid.readNdefData().then(function(data) {
+			if (data === undefined) {
+				console.log("**** UNDEFINED ****");
+				for (var i = 0; i < 5; i++) {
+					redLight(i);
+				}
+
+			} else {
+				var records = ndef.decodeMessage(Array.from(data));
+				var cardscan = JSON.parse(JSON.stringify(records))[1]['value'].toString();
+				var isValid = Gatekeeper.authorizeAddress(cardscan);
+				if (isValid) {
+					console.log("**** OPEN GATE ****");
+					redled.writeSync(0);
+					greenled.writeSync(1);
+					spawn('python',["opendoor.py"]);
+					setTimeout(function() {
+						console.log("**** CLOSE GATE ****");
+						redled.writeSync(1);
+						greenled.writeSync(0);
+						spawn('python',["closedoor.py"]);
+					}, 5000)
+				} else {
+					console.log("***** INVALID *****");
+					for (var i = 0; i < 7; i++) {
+						redLight(i);
+					}
+				}
+			}
+        });
+    });
 });
 
 
 process.on('SIGINT', function () {
 	redled.unexport();
 	greenled.unexport();
-	button.unexport();
 });
