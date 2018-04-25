af267098a580fe89edb781555c4a76497c3c2bf3
Eduardo Portet <eportet@dhcp-wifi-8021x-155-41-23-103.bu.edu>
2018-04-25 15:43:05 -0400

    Separate functions

--- opendoor.py
+++ opendoor.py
@@ -5,26 +5,16 @@ DIR = 20   # Direction GPIO Pin
 STEP = 21  # Step GPIO Pin
 
 CCW = 1   # CounterClockwise Rotation
-CW = 0   # Clockwise Rotation
 SPR = 400   # Steps per Revolution (360 / 7.5)
 #//1000 full rot?? play with
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
+GPIO.output(DIR, CCW)
 
 step_count = SPR
 delay = 0.005 #keep above 001 #smaller turns faster
-
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-
-sleep(1)
-GPIO.output(DIR, CCW)
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
