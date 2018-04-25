adfab47236426781f2daf0e8125cf47ed87b069d
Eduardo Portet <eportet@dhcp-wifi-8021x-155-41-23-103.bu.edu>
2018-04-25 17:51:13 -0400

    change styling

--- node/gatekeep.js
+++ node/gatekeep.js
@@ -1,80 +1,105 @@
+// Dependencies
+var Web3 = require('web3');
 var Gpio = require('onoff').Gpio;
 var SerialPort = require('serialport');
 var pn532 = require('pn532');
 var ndef = require('ndef');
 var spawn = require("child_process").spawn;
 
+// Initializing variables
 var greenled = new Gpio(23, 'out');
 var redled = new Gpio(24, 'out');
 var port = new SerialPort('/dev/ttyS0', { baudRate: 115200 });
 var rfid = new pn532.PN532(port);
-
-var Web3 = require('web3');
 var web3 = new Web3();
+
+// Attach to local Geth RPC
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
+// Check if successfuly connected to Ethereum
 if (web3.isConnected()){
     console.log("Ethereum node successfully connected to version:", web3.version.node);
+} else {
+    console.log("ERROR: Could not connect to the Ethereum network")
 }
 
+// Get this device's Wallet ID
 var coinbase = web3.eth.accounts[0];
 console.log("Using Wallet ID: " + coinbase);
 
+// Set as default account
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
-var GatekeeperABIString = '[ { "constant": true, "inputs": [ { "name": "_address", "type": "address" } ], "name": "authorizeAddress", "outputs": [ { "name": "isValid", "type": "bool", "value": false } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]';
-var GatekeeperABI = JSON.parse(GatekeeperABIString);
+// Connect to SmartContract
+var GatekeeperABI = JSON.parse('[ { "constant": true, "inputs": [ { "name": "_address", "type": "address" } ], "name": "authorizeAddress", "outputs": [ { "name": "isValid", "type": "bool", "value": false } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]');
 var GatekeeperContractAddress = '0xd189CaB96c0ee645e6eE80BE79979FaDccfe1055';
 var Gatekeeper = web3.eth.contract(GatekeeperABI).at(GatekeeperContractAddress);
 
+// Turn on LED lights
 redled.writeSync(1);
 greenled.writeSync(0);
 
-console.log('\n********* READY *********');
-
-function redLight(i) {
-	setTimeout(function() { redled.writeSync((i+1)%2); }, 250*i)
-}
-
 rfid.on('ready', function() {
-    console.log('Listening for a tag scan...');
-    rfid.on('tag', function(tag) {
-        console.log('Reading tag data...');
-        rfid.readNdefData().then(function(data) {
-			if (data === undefined) {
-				console.log("**** UNDEFINED ****");
-				for (var i = 0; i < 5; i++) {
-					redLight(i);
-				}
+  // RFID Reader Identified
+  console.log('\n********* READY *********');
+  var finish = true;
+  if (finish) {
+    finish = false;
+    scanTag(finish);
+  }
 
-			} else {
-				var records = ndef.decodeMessage(Array.from(data));
-				var cardscan = JSON.parse(JSON.stringify(records))[1]['value'].toString();
-				var isValid = Gatekeeper.authorizeAddress(cardscan);
-				if (isValid) {
-					console.log("**** OPEN GATE ****");
-					redled.writeSync(0);
-					greenled.writeSync(1);
-					spawn('python',["motor/opendoor.py"]);
-					setTimeout(function() {
-						console.log("**** CLOSE GATE ****");
-						redled.writeSync(1);
-						greenled.writeSync(0);
-						spawn('python',["motor/closedoor.py"]);
-					}, 5000)
-				} else {
-					console.log("***** INVALID *****");
-					for (var i = 0; i < 7; i++) {
-						redLight(i);
-					}
-				}
-			}
-        });
-    });
 });
 
+function scanTag(finish) {
+  rfid.on('tag', function(tag) {
+    console.log('Reading tag data...');
+    rfid.readNdefData().then(function(data) {
+      if (data === undefined) {
+        console.log("**** UNDEFINED ****");
+        blinkRedLight(2, finish);
+      } else {
+        var records = ndef.decodeMessage(Array.from(data));
+        var cardscan = JSON.parse(JSON.stringify(records))[1]['value'].toString();
+        var isValid = Gatekeeper.authorizeAddress(cardscan);
+        if (isValid) {
+          openGate(finish);
+        } else {
+          console.log("***** INVALID *****");
+          blinkRedLight(3,finish);
+        }
+      }
+    });
+  });
+}
+
+function openGate(finish) {
+  console.log("**** OPEN GATE ****");
+  redled.writeSync(0);
+  greenled.writeSync(1);
+  spawn('python',["motor/opendoor.py"]);
+
+  setTimeout(function() {
+    console.log("**** CLOSE GATE ****");
+    redled.writeSync(1);
+    greenled.writeSync(0);
+    spawn('python',["motor/closedoor.py"]);
+
+    finish = true;
+  }, 5000)
+}
+
+function redLight(i) {
+  setTimeout(function() { redled.writeSync((i+1)%2); }, 250*i)
+}
+
+function blinkRedLight(n, finish) {
+  for (var i = 0; i < ((n*2)+1); i++) {
+    redLight(i);
+  }
+  setTimeout(function() { finish = true; }, 250*2*n)
+}
 
 process.on('SIGINT', function () {
-	redled.unexport();
-	greenled.unexport();
+  redled.unexport();
+  greenled.unexport();
 });
--- solidity/Gatekeeper.sol
+++ solidity/Gatekeeper.sol
@@ -2,32 +2,31 @@ pragma solidity ^0.4.0;
 
 contract Gatekeeper {
 
-    struct Account {
-        bool isValid;
-        string fName;
-        string lName;
-    }
+  struct Account {
+    bool isValid;
+    string fName;
+    string lName;
+  }
 
-    mapping (address => Account) accounts;
-    address[] private authorizedAccts;
+  mapping (address => Account) accounts;
+  address[] private authorizedAccts;
 
-    function Gatekeeper() public{
-	    setAccount(0xa1b29Ae29C6A877383Ad50F5a66EE2Ef0a75e84E, "Eduardo", "Portet");
-	}
+  function Gatekeeper() public{
+    setAccount(0xa1b29Ae29C6A877383Ad50F5a66EE2Ef0a75e84E, "Eduardo", "Portet");
+  }
 
-    function setAccount(address _address, string _fName, string _lName) private {
-        Account storage account = accounts[_address];
+  function setAccount(address _address, string _fName, string _lName) private {
+    Account storage account = accounts[_address];
 
-        account.isValid = true;
-        account.fName = _fName;
-        account.lName = _lName;
+    account.isValid = true;
+    account.fName = _fName;
+    account.lName = _lName;
 
-        authorizedAccts.push(_address) -1;
-    }
+    authorizedAccts.push(_address) -1;
+  }
 
-	function authorizeAddress(address _address) public constant returns (bool){
-        Account storage account = accounts[_address];
-        return account.isValid;
-
-	}
+  function authorizeAddress(address _address) public constant returns (bool){
+    Account storage account = accounts[_address];
+    return account.isValid;
+  }
 }
