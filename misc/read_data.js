a0380deaf48dc28f4f74aac6c3b0fa127b771e0d
Eduardo Portet <eduardoportet@gmail.com>
2018-04-29 01:03:32 -0400

    data read add

---
+++ misc/read_data.js
@@ -0,0 +1,19 @@
+var pn532 = require('pn532');
+var SerialPort = require('serialport');
+
+var serialPort = new SerialPort('/dev/ttyS0', { baudRate: 115200 });
+var rfid = new pn532.PN532(serialPort);
+var ndef = require('ndef');
+
+console.log('Waiting for rfid ready event...');
+rfid.on('ready', function() {
+
+    console.log('Listening for a tag scan...');
+    rfid.on('tag', function(tag) {
+        rfid.readNdefData().then(function(data) {
+            var records = ndef.decodeMessage(Array.from(data));
+			var r = JSON.parse(JSON.stringify(records));
+			console.log(records);
+        });
+    });
+});
